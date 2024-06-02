f8c595f3c4e70b920f4988e966dfc91bd008f1d1
blather-void <brigacademia@gmail.com>
2024-06-02 10:50:11 -0400

    snippets added

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,19 +18,19 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
--- some ultisnips commands
+-- some ultisnips commands. Don't move these it doesnt work for some reason. throws a "cannot find luasnip" error and cba to figure that out 
 vim.cmd[[
-let g:UltiSnipsExpandOrJumpTrigger = '<Tab>'
+let g:UltiSnipsJumpForwardTrigger= '<Tab>'
 let g:UltiSnipsJumpBackwardTrigger = '<S-Tab>'
 let g:UltiSnipsSnippetDirectories=[$HOME.'/.config/nvim/UltiSnips']
+nnoremap <leader>u <Cmd>call UltiSnips#RefreshSnippets()<CR>
 ]]
 
--- and now time for vimtex lol 
+-- and now time for vimtex lol (same as above for moving it ffs)
 vim.cmd[[
 filetype plugin indent on
-
+let g:tex_flavor='latex'
 let g:vimtex_view_method = 'zathura'
 let g:vimtex_compiler_method = 'latexmk'
-
 let maplocalleader = ' '
 ]]
